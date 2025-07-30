1c223ea8e73b1ba1a6ad6e98de135eec64aa4d9d
AgstnWidyas <widyaagstn111@gmail.com>
2025-07-30 20:29:15 +0700

    update lagi

--- src_corrector/accuration_llm_whisper.py
+++ src_corrector/accuration_llm_whisper.py
@@ -2,8 +2,8 @@ import os
 import pandas as pd
 from faster_whisper import WhisperModel
 from pydub import AudioSegment
-#from llm_corrector_phi2 import correct_text 
-from llm_corrector_gemma2B import correct_text 
+from llm_corrector_phi2 import correct_text 
+#from llm_corrector_gemma2B import correct_text 
 #from llm_corrector_tinyllama import correct_text
 from jiwer import wer, Compose, ToLowerCase, RemovePunctuation, RemoveMultipleSpaces, RemoveWhiteSpace, ExpandCommonEnglishContractions
 
